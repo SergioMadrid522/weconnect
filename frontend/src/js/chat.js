6dbf3ff535df6f4debb9bff221efe1481af576df
SergioMadrid522 <Sergiofabian.madrid@hotmail.com>
2025-06-27 20:00:31 -0600

    Fix: chat and add styling on chat page

--- frontend/src/js/chat.js
+++ frontend/src/js/chat.js
@@ -3,18 +3,22 @@ form.addEventListener("submit", (e) => {
   e.preventDefault();
 
   const chat = document.querySelector(".chat");
-  //const userName = document.getElementById("user-name");
 
   const message = document.createElement("div");
   message.classList.add("msg");
 
-  const msg = document.getElementById("message-input").value;
+  const msgInput = document.getElementById("message-input");
+  const msg = msgInput.value;
 
   if (msg === "") {
     alert("msg empty");
     return;
   }
+
   //message.innerHTML = `${userName}: ${msg}`;
   message.innerHTML = msg;
   chat.appendChild(message);
+
+  //Clean the message input
+  msgInput.value = "";
 });
